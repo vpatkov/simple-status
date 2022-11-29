ce9e4f01b6c0227a353f031c8bb018239acdf819
Vyacheslav Patkov <slava@patkov.ru>
2022-11-29 19:53:58 +0400

    Fix block.full_text in network module

--- modules/network.c
+++ modules/network.c
@@ -36,13 +36,11 @@ static char *find_default_iface(void) {
 
 struct block *network_update(void) {
         static char full_text[32];
-        static struct block block = {
-                .full_text = full_text,
-        };
+        static struct block block;
 
         char *iface = find_default_iface();
         if (strcmp(iface, "lo") == 0) {
-                block.full_text = "NET  DISCONNECTED  ";
+                block.full_text = "NET DISCONNECTED";
                 block.urgent = true;
                 return &block;
         }
@@ -55,14 +53,14 @@ struct block *network_update(void) {
         if (pscanf(rx_path, "%llu", &rx_bytes) != 1 ||
             pscanf(tx_path, "%llu", &tx_bytes) != 1) {
                 error("network: pscanf() failed.");
-                *full_text = 0;
+                block.full_text = "";
                 return &block;
         }
 
         struct timespec ts;
         if (clock_gettime(CLOCK_MONOTONIC_RAW, &ts) == -1) {
                 error("network: clock_gettime() failed: %s.", strerror(errno));
-                *full_text = 0;
+                block.full_text = "";
                 return &block;
         }
 
@@ -82,8 +80,10 @@ struct block *network_update(void) {
         print_with_suffix(rx_speed_text, size(rx_speed_text), rx_speed);
         print_with_suffix(tx_speed_text, size(tx_speed_text), tx_speed);
 
-        block.urgent = false;
         snprintf(full_text, size(full_text), "NET %s↓ %s↑ B/s",
                 rx_speed_text, tx_speed_text);
+
+        block.full_text = full_text;
+        block.urgent = false;
         return &block;
 }
